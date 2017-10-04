c270aa975754ac110757742ccc015a2323818894
vlpekov <vladdey@gmail.com>
2017-10-04 15:21:10 +0300

    mock data: requests[], users[]

---
+++ src/app/models/mock-request.ts
@@ -0,0 +1,89 @@
+import { Request } from "./request";
+
+export const REQUESTS: Request[] = [
+  {
+    request_id: 1,
+    type: "PTO",
+    days: 4,
+    date_start: "13.11.2017",
+    date_end: "16.11.2017",
+    status: "approved",
+    reason: "I'm going safari to Africa.",
+    note: "It's going to be dangerous.",
+    submit_date: "02.10.2017"
+  },
+  {
+    request_id: 2,
+    type: "PTO",
+    days: 10,
+    date_start: "6.12.2017",
+    date_end: "17.12.2017",
+    status: "approved",
+    reason: "Holyday",
+    note: "Don't call me.",
+    submit_date: "05.10.2017"
+  },
+  {
+    request_id: 3,
+    type: "Sick Leave",
+    days: 3,
+    date_start: "6.10.2017",
+    date_end: "10.10.2017",
+    status: "approved",
+    reason: "Fly",
+    note: "",
+    submit_date: "06.10.2017"
+  },
+  {
+    request_id: 4,
+    type: "UPTO",
+    days: 1,
+    date_start: "13.10.2017",
+    date_end: "16.11.2017",
+    status: "approved",
+    reason: "I'm superstitious.",
+    note: "It's Friday 13th.",
+    submit_date: "02.10.2017"
+  },   {
+    request_id: 5,
+    type: "PTO",
+    days: 4,
+    date_start: "12.11.2017",
+    date_end: "16.11.2017",
+    status: "approved",
+    reason: "I'm going safari to Africa.",
+    note: "It's going to be dangerous.",
+    submit_date: "02.10.2017"
+  },   {
+    request_id: 6,
+    type: "PTO",
+    days: 4,
+    date_start: "12.11.2017",
+    date_end: "16.11.2017",
+    status: "approved",
+    reason: "I'm going safari to Africa.",
+    note: "It's going to be dangerous.",
+    submit_date: "02.10.2017"
+  },   {
+    request_id: 7,
+    type: "PTO",
+    days: 4,
+    date_start: "12.11.2017",
+    date_end: "16.11.2017",
+    status: "approved",
+    reason: "I'm going safari to Africa.",
+    note: "It's going to be dangerous.",
+    submit_date: "02.10.2017"
+  },
+  {
+    request_id: 8,
+    type: "PTO",
+    days: 4,
+    date_start: "12.11.2017",
+    date_end: "16.11.2017",
+    status: "approved",
+    reason: "I'm going safari to Africa.",
+    note: "It's going to be dangerous.",
+    submit_date: "02.10.2017"
+  }
+];
---
+++ src/app/models/mock-user.ts
@@ -0,0 +1,19 @@
+import { User } from "./user";
+
+export const USERS: User[] = [
+    {
+        user_id: 1,
+        person_id: "Todor Kostadinov",
+        first_name: "Todor",
+        second_name: "Todorov",
+        last_name: "Kostadinov",
+        is_admin: false,
+        username: "todor",
+        password: "password",
+        position: "Software developer",
+        email: "todor_kostadinov@gmail.com",
+        available_pto: 11,
+        all_pto: 20
+      },
+];
+
