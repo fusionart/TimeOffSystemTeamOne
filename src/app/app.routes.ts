d00112a6dc27c620815e929bb151e57d7ee95b42
[fusionart] <darkart@abv.bg>
2017-10-05 19:32:38 +0300

    merge problem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 =======
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
->>>>>>> bca37a6102db73c71dff651f0b184f196258e27c
 
 export const appRoutes: Routes = [];
 
