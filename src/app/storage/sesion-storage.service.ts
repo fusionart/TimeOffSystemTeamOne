3b3316b3e800c31ea7ae5b332dd1f371c8621a7d
vlpekov <vladdey@gmail.com>
2017-10-19 08:56:09 +0300

    backup

--- src/app/storage/sesion-storage.service.ts
+++ src/app/storage/sesion-storage.service.ts
@@ -4,50 +4,49 @@ import { User } from '../models/user';
 @Injectable()
 export class SesionStorageService {
 
-  storage:User;
+  // storage:User;
 
-  constructor()
-  {
-    this.storage = new User();
-  }
+  // constructor()
+  // {
+  //   this.storage = new User();
+  // }
 
-  getUsers()
-  {
-    this.storage = JSON.parse(localStorage.getItem('storage'));
-    return this.storage;
-  }
+  // getUsers()
+  // {
+  //   this.storage = JSON.parse(localStorage.getItem('storage'));
+  //   return this.storage;
+  // }
 
-  addUser(user:User)
-  {
-    this.storage.push(user);
+  // addUser(user:User)
+  // {
+  //   this.storage.push(user);
 
-    let users;
+  //   let users;
 
-    if(localStorage.getItem('storage') === null)
-    {
-      users = [];
-      users.push(user);
-      localStorage.setItem('storage', JSON.stringify(users));
-    } 
-    else
-    {
-      users = JSON.parse(localStorage.getItem('storage'));
-      users.push(user);
-      localStorage.setItem('storage', JSON.stringify(users));
-    }
-  }
+  //   if(localStorage.getItem('storage') === null)
+  //   {
+  //     users = [];
+  //     users.push(user);
+  //     localStorage.setItem('storage', JSON.stringify(users));
+  //   } 
+  //   else
+  //   {
+  //     users = JSON.parse(localStorage.getItem('storage'));
+  //     users.push(user);
+  //     localStorage.setItem('storage', JSON.stringify(users));
+  //   }
+  // }
 
-  removeUser(user:User)
-  {
-    for(let i = 0; i < this.storage.length; i++)
-    {
-      if(user == this.storage[i])
-      {
-        this.storage.splice(i, 1);
-        localStorage.setItem('storage', JSON.stringify(this.storage));
-      }
-    }
-    return this.storage
-  }
-}
+  // removeUser(user:User)
+  // {
+  //   for(let i = 0; i < this.storage.length; i++)
+  //   {
+  //     if(user == this.storage[i])
+  //     {
+  //       this.storage.splice(i, 1);
+  //       localStorage.setItem('storage', JSON.stringify(this.storage));
+  //     }
+  //   }
+  //   return this.storage
+  // }
 }
