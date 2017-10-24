d94a9fc88926892b4c046f6d465f3ee3afe26580
DESKTOP-DOO9V8G\Petra <petrasmile.1@gmail.com>
2017-10-24 22:03:54 +0300

    Fixed service

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -11,7 +11,7 @@ import { User } from "./../../models/user";
 export class RegisterFormComponent {
 
   constructor(
-    private registerUserService: RegisterUserService;
+    private registerUserService: RegisterUserService
   ) { }
 
   OnSubmit(form: NgForm) {
